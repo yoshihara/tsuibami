566c9101a27a0c38956304223df8d54eda4d7ce4
yoshihara <h.yoshihara@everyleaf.com>
2015-11-08 16:58:34 +0900

    Get team's icon when configuration is saved

--- src/js/options.js
+++ src/js/options.js
@@ -1,19 +1,40 @@
-// TODO saveしたときにアイコンを変えたい
+getTeamIcon = function(config) {
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.esa.io/v1/teams/" + config.teamName,
+            data: {
+                access_token: config.token
+            },
+            success: function(response) {
+                config.teamIcon = response.icon;
+                resolve(config);
+            },
+            error: reject
+        });
+    });
+}
+
+saveConfig = function(config) {
+    chrome.storage.sync.set(config, function(){
+        $(".message").text("saved!");
+        $(".icon")[0].src = config.teamIcon;
+    });
+}
+
 $(function() {
-    var defaultConfig = {teamName: "", token: ""};
+    var defaultConfig = {teamName: "", token: "", teamIcon: ""};
     chrome.storage.sync.get(defaultConfig, function(config) {
         $(".team-name").val(config.teamName);
         $(".token").val(config.token);
-
+        $(".icon")[0].src = config.teamIcon;
     });
 
     $(".save").on("click", function() {
-        config = {
+        var config = {
             teamName: $(".team-name").val(),
             token: $(".token").val()
         };
-        chrome.storage.sync.set(config, function(){
-            $(".message").text("saved!");
-        });
+        getTeamIcon(config).then(saveConfig);
     });
 });
