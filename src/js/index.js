a82da7aa73b79385676c8d96221d59616bf2f855
yoshihara <yoshihara@users.noreply.github.com>
2018-07-19 17:58:11 +0900

    Move main process from index to Popup

--- src/js/index.js
+++ src/js/index.js
@@ -5,197 +5,45 @@ import _ from 'lodash';
 import Promise from 'bluebird';
 import '../../lib/jquery.selection';
 import '../../lib/jquery.esarea';
+import Popup from './popup';
 import Ui from './ui';
 import Post from './post';
 import Esa from './esa';
 
-let ui = new Ui();
-let post = new Post();
-let esa;
-
-const setPreviousPost = function() {
-  Post.load(function(loadedPost) {
-    post = loadedPost;
-    ui.toggleDisabledSaveButton(post.saved);
-
-    ui.title = post.title;
-    ui.body = post.body;
-    if (ui.title != '') {
-      // Move cursor at previous position
-      ui.cursorPosition = post.cursorPosition;
-    }
-  });
-};
-
-const setPreviousState = function() {
-  return new Promise(function(resolve, reject) {
-    let defaultConfig = {
-      teamName: '',
-      token: '',
-      teamIcon: '',
-      savedPostLink: '',
-      postId: '',
-    };
-    chrome.storage.sync.get(defaultConfig, function(config) {
-      if (!config.teamName || !config.token) {
-        ui.toggleDisplayOptionLink(true);
-        ui.teamName = 'Configuration failed';
-
-        reject('Please configure options (\\( ˘⊖˘)/)');
-      }
-
-      esa = new Esa(config);
-
-      ui.toggleDisplayOptionLink(false);
-
-      if (config.savedPostLink != '') {
-        ui.savedPostLink = config.savedPostLink;
-      } else {
-        // NOTE: 0.2.1以前の後方互換性のための対応
-        // TODO: 次々回のリリースで削除する（storageから取得する部分も一緒に削除する）
-        ui.savedPostLink = `https://${config.teamName}.esa.io/posts/${
-          config.postId
-        }`;
-      }
-
-      ui.teamName = config.teamName;
-      ui.teamIcon = config.teamIcon;
-
-      resolve();
-    });
-  });
-};
-
-const searchPost = function() {
-  return new Promise(function(resolve, reject) {
-    let [category, title] = Post.splitCategory(ui.title);
-
-    let q = `name:${title}`;
-    if (category.length != 0) q = `${q} category:${category}`;
-
-    let filterPostsFunc = function(response) {
-      // nameによる検索は部分一致のため、完全一致させるために検索結果から更に絞り込んでいる
-      let hitPost = Post.filterPosts(title, category, response);
-
-      let postId = hitPost ? hitPost.number : undefined;
-      resolve(postId);
-    };
-
-    esa.search(q, filterPostsFunc, reject);
-  });
-};
-
-const savePost = function(postId) {
-  return new Promise(function(resolve, reject) {
-    let [category, title] = Post.splitCategory(ui.title);
-
-    let postData = {
-      id: postId,
-      name: title,
-      category: category,
-      body_md: ui.body,
-      message: 'from tsuibami',
-    };
-
-    esa.save(postData, resolve, reject);
-  });
-};
-
-const updateStoredPost = function(response) {
-  post.saved = true;
-  post.savedPostLink = response.url;
-  if (ui.isClearCheckBoxChecked) {
-    post.title = '';
-    post.body = '';
-    post.cursorPosition = 0;
-  } else {
-    // 保存したタイトルによっては末尾に "(2)" などの表記がesaによってつけられていたり、タイトル無しで保存した場合にカテゴリが変わっている可能性があるため、内容を更新する
-    post.title = response.full_name;
-  }
-
-  return new Promise(function(resolve, reject) {
-    post.store(
-      () => {
-        resolve(response);
-      },
-      () => {
-        reject(response);
-      },
-    );
-  });
-};
-
-const updateUI = function(response) {
-  ui.toggleDisabledSaveButton(true);
-
-  ui.title = post.title;
-  ui.body = post.body;
-
-  ui.savedPostLink = post.savedPostLink;
-
-  let message;
-  // 最初の保存の時だけメッセージを変える
-  if (response.revision_number == 1) {
-    message = 'created!';
-  } else {
-    message = 'updated!';
-  }
-  notifySuccess(message);
-};
-
-const notifySuccess = function(msg) {
-  showMessage(msg + ' (\\( ⁰⊖⁰)/)', true);
-};
-
-const notifyError = function(msg) {
-  console.log(msg);
-  let message = msg.hasOwnProperty('statusText') ? msg.statusText : msg;
-  message = msg.hasOwnProperty('status')
-    ? `${message} (${msg.status})`
-    : message;
-  showMessage(message);
-};
-
-const showMessage = function(message, succeeded) {
-  if (succeeded) {
-    ui.messageArea.showMessageOnSuccess(message);
-  } else {
-    ui.messageArea.showMessageOnFailure(message);
-  }
-};
+const popup = new Popup();
 
 const storeTitle = function() {
-  let title = ui.title;
+  let title = popup.ui.title;
 
-  if (title != post.title) {
-    post.title = title;
-    post.saved = false;
-    post.store(function() {}, showErrorMessage);
-    ui.toggleDisabledSaveButton(false);
+  if (title != popup.post.title) {
+    popup.post.title = title;
+    popup.post.saved = false;
+    popup.post.store(function() {}, showErrorMessage);
+    popup.ui.toggleDisabledSaveButton(false);
   }
 };
 
 const storeBody = function() {
-  let body = ui.body;
+  let body = popup.ui.body;
 
-  if (body != post.body) {
-    post.body = body;
-    post.saved = false;
-    ui.toggleDisabledSaveButton(false);
+  if (body != popup.post.body) {
+    popup.post.body = body;
+    popup.post.saved = false;
+    popup.ui.toggleDisabledSaveButton(false);
   }
 
-  let cursorPosition = ui.cursorPosition;
-  if (cursorPosition != post.cursorPosition) {
-    post.cursorPosition = cursorPosition;
+  let cursorPosition = popup.ui.cursorPosition;
+  if (cursorPosition != popup.post.cursorPosition) {
+    popup.post.cursorPosition = cursorPosition;
   }
-  post.store(function() {}, showErrorMessage);
+  popup.post.store(function() {}, showErrorMessage);
 };
 
 const storeCursorPosition = function() {
-  let cursorPosition = ui.cursorPosition;
-  if (cursorPosition != post.cursorPosition) {
-    post.cursorPosition = cursorPosition;
-    post.store(function() {}, showErrorMessage);
+  let cursorPosition = popup.ui.cursorPosition;
+  if (cursorPosition != popup.post.cursorPosition) {
+    popup.post.cursorPosition = cursorPosition;
+    popup.post.store(function() {}, showErrorMessage);
   }
 };
 
@@ -204,42 +52,29 @@ const showErrorMessage = function() {
   if (chrome.runtime.lastError === undefined) message = 'unknown error';
   else message = chrome.runtime.lastError.message;
 
-  showMessage('Error: ' + message, false);
-};
-
-const runSaveProcess = function() {
-  ui.toggleDisabledSaveButton(true);
-  ui.toggleSavedStatusMessage(true);
-  searchPost()
-    .then(savePost)
-    .then(updateStoredPost)
-    .then(updateUI)
-    .catch(notifyError)
-    .finally(function() {
-      ui.toggleSavedStatusMessage(false);
-    });
+  popup.showMessage('Error: ' + message, false);
 };
 
 const runSaveProcessByShortcut = function(event) {
   // Ctrl+s or Cmd+s
   if ((event.metaKey || event.ctrlKey) && event.keyCode == 83) {
     event.preventDefault();
-    runSaveProcess();
+    popup.runSaveProcess();
   }
 };
 
 $(function() {
-  setPreviousPost();
-  setPreviousState().catch(notifyError);
+  popup.setPreviousPost();
+  popup.setPreviousState().catch(popup.notifyError);
 
-  ui.titleDom.on('keyup', _.debounce(storeTitle, 200));
-  ui.bodyDom.on('keyup', _.debounce(storeBody, 200));
-  ui.bodyDom.on('mouseup', storeCursorPosition);
+  popup.ui.titleDom.on('keyup', _.debounce(storeTitle, 200));
+  popup.ui.bodyDom.on('keyup', _.debounce(storeBody, 200));
+  popup.ui.bodyDom.on('mouseup', storeCursorPosition);
 
-  ui.titleDom.on('keydown', runSaveProcessByShortcut);
-  ui.bodyDom.on('keydown', runSaveProcessByShortcut);
+  popup.ui.titleDom.on('keydown', runSaveProcessByShortcut);
+  popup.ui.bodyDom.on('keydown', runSaveProcessByShortcut);
 
-  ui.bodyDom.esarea();
+  popup.ui.bodyDom.esarea();
 
-  ui.postButtonDom.on('click', runSaveProcess);
+  popup.ui.postButtonDom.on('click', popup.runSaveProcess.bind(popup));
 });
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,182 @@
 'use strict';
 
-export default class Popup {}
+import $ from 'jquery';
+import _ from 'lodash';
+import Promise from 'bluebird';
+
+import Ui from './ui';
+import Post from './post';
+import Esa from './esa';
+
+export default class Popup {
+  constructor() {
+    this.ui = new Ui();
+    this.post = new Post();
+    this.esa = undefined;
+  }
+
+  setPreviousPost() {
+    Post.load((loadedPost) => {
+      this.post = loadedPost;
+      this.ui.toggleDisabledSaveButton(this.post.saved);
+
+      this.ui.title = this.post.title;
+      this.ui.body = this.post.body;
+      if (this.ui.title != '') {
+        // Move cursor at previous position
+        this.ui.cursorPosition = this.post.cursorPosition;
+      }
+    });
+  }
+
+  setPreviousState() {
+    return new Promise((resolve, reject) => {
+      let defaultConfig = {
+        teamName: '',
+        token: '',
+        teamIcon: '',
+        savedPostLink: '',
+        postId: '',
+      };
+      chrome.storage.sync.get(defaultConfig, (config) => {
+        if (!config.teamName || !config.token) {
+          this.ui.toggleDisplayOptionLink(true);
+          this.ui.teamName = 'Configuration failed';
+
+          reject('Please configure options (\\( ˘⊖˘)/)');
+        }
+
+        this.esa = new Esa(config);
+
+        this.ui.toggleDisplayOptionLink(false);
+
+        if (config.savedPostLink != '') {
+          this.ui.savedPostLink = config.savedPostLink;
+        } else {
+          // NOTE: 0.2.1以前の後方互換性のための対応
+          // TODO: 次々回のリリースで削除する（storageから取得する部分も一緒に削除する）
+          this.ui.savedPostLink = `https://${config.teamName}.esa.io/posts/${
+            config.postId
+          }`;
+        }
+
+        this.ui.teamName = config.teamName;
+        this.ui.teamIcon = config.teamIcon;
+
+        resolve();
+      });
+    });
+  }
+
+  runSaveProcess() {
+    this.ui.toggleDisabledSaveButton(true);
+    this.ui.toggleSavedStatusMessage(true);
+
+    this.searchPost()
+      .then(this.savePost.bind(this))
+      .then(this.updateStoredPost.bind(this))
+      .then(this.updateUI.bind(this))
+      .catch(this.notifyError.bind(this))
+      .finally(() => {
+        this.ui.toggleSavedStatusMessage(false);
+      });
+  }
+
+  searchPost() {
+    return new Promise((resolve, reject) => {
+      let [category, title] = Post.splitCategory(this.ui.title);
+
+      let q = `name:${title}`;
+      if (category.length != 0) q = `${q} category:${category}`;
+
+      let filterPostsFunc = (response) => {
+        // nameによる検索は部分一致のため、完全一致させるために検索結果から更に絞り込んでいる
+        let hitPost = Post.filterPosts(title, category, response);
+
+        let postId = hitPost ? hitPost.number : undefined;
+        resolve(postId);
+      };
+
+      this.esa.search(q, filterPostsFunc, reject);
+    });
+  }
+
+  savePost(postId) {
+    return new Promise((resolve, reject) => {
+      let [category, title] = Post.splitCategory(this.ui.title);
+
+      let postData = {
+        id: postId,
+        name: title,
+        category: category,
+        body_md: this.ui.body,
+        message: 'from tsuibami',
+      };
+
+      this.esa.save(postData, resolve, reject);
+    });
+  }
+
+  updateStoredPost(response) {
+    this.post.saved = true;
+    this.post.savedPostLink = response.url;
+    if (this.ui.isClearCheckBoxChecked) {
+      this.post.title = '';
+      this.post.body = '';
+      this.post.cursorPosition = 0;
+    } else {
+      // 保存したタイトルによっては末尾に "(2)" などの表記がesaによってつけられていたり、タイトル無しで保存した場合にカテゴリが変わっている可能性があるため、内容を更新する
+      this.post.title = response.full_name;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.post.store(
+        () => {
+          resolve(response);
+        },
+        () => {
+          reject(response);
+        },
+      );
+    });
+  }
+
+  updateUI(response) {
+    this.ui.toggleDisabledSaveButton(true);
+
+    this.ui.title = this.post.title;
+    this.ui.body = this.post.body;
+
+    this.ui.savedPostLink = this.post.savedPostLink;
+
+    let message;
+    // 最初の保存の時だけメッセージを変える
+    if (response.revision_number == 1) {
+      message = 'created!';
+    } else {
+      message = 'updated!';
+    }
+    this.notifySuccess(message);
+  }
+
+  notifySuccess(msg) {
+    this.showMessage(msg + ' (\\( ⁰⊖⁰)/)', true);
+  }
+
+  notifyError(msg) {
+    console.log(msg);
+    let message = msg.hasOwnProperty('statusText') ? msg.statusText : msg;
+    message = msg.hasOwnProperty('status')
+      ? `${message} (${msg.status})`
+      : message;
+    this.showMessage(message);
+  }
+
+  showMessage(message, succeeded) {
+    if (succeeded) {
+      this.ui.messageArea.showMessageOnSuccess(message);
+    } else {
+      this.ui.messageArea.showMessageOnFailure(message);
+    }
+  }
+}
